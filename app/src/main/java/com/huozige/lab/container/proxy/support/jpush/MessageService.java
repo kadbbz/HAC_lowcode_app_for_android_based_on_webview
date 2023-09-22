b3a21eaa76a11b34a12ca3e3f8c37776162af10f
NingWei <will.ning@grapecity.com>
2023-09-22 18:08:01 +0800

    接入极光推送

---
+++ app/src/main/java/com/huozige/lab/container/proxy/support/jpush/MessageService.java
@@ -0,0 +1,8 @@
+package com.huozige.lab.container.proxy.support.jpush;
+
+import cn.jpush.android.service.JPushMessageService;
+
+public class MessageService extends JPushMessageService {
+    public MessageService() {
+    }
+}
